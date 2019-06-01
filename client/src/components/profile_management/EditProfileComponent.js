eb7f5758b401900fa3994434d786a2e99ed64514
celif <cfeliciano8791@gmail.com>
2019-06-01 15:15:39 -0400

    Fixed the minor bug related to unwanted characters in the input

--- client/src/components/profile_management/EditProfileComponent.js
+++ client/src/components/profile_management/EditProfileComponent.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Form, Alert, Button, Container } from "react-bootstrap";
 import {Grid, Col, Dropdown, DropdownButton} from "react-bootstrap";
 
-// TODO(celif): Fix integer input accepting +, -, ., floating point numbers, breaking input.
-// https://stackoverflow.com/questions/52777762/html5-input-type-number-not-prevent-plus-minus-symbols
-
 function EditProfileComponent() {
 
     const months = ["January", "February", "March", "April", "May", "June", 
@@ -15,9 +12,17 @@ function EditProfileComponent() {
     }
 
     function checkLength(event) {
-        if (event.currentTarget.value.length > event.currentTarget.maxLength) {
-            event.currentTarget.value = event.currentTarget.value.slice(0, event.currentTarget.maxLength)
-        }
+       let inString = event.currentTarget.value
+       let inChar = (inString).charCodeAt(inString.length-1)
+       
+       if (inChar < 48 || inChar > 57) {
+           event.currentTarget.value = inString.slice(0, inString.length-1)
+       }
+       
+       if (inString.length > event.currentTarget.maxLength) {
+            event.currentTarget.value = inString.slice(0, event.currentTarget.maxLength)
+       }
+
     }
 
 
@@ -33,7 +38,7 @@ function EditProfileComponent() {
                 <b>Add a credit card</b>
                 <Form.Group controlId="EditProfileComponent.creditCardNumber">
                     <Form.Label>Credit Card Number</Form.Label>
-                    <Form.Control type="number" maxlength="16" onInput={checkLength}/>
+                    <Form.Control type="text" maxlength="16" onInput={checkLength}/>
                  </Form.Group>
                  <Form.Label>Expiration Date</Form.Label>
                  <Form.Row controlId="EditProfileComponent.expirationDate">
@@ -45,12 +50,12 @@ function EditProfileComponent() {
                            </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col} md="4">    
-                        <Form.Control type="number" maxlength="4" onInput={checkLength} placeholder="Year"/>
+                        <Form.Control type="text" maxlength="4" onInput={checkLength} placeholder="Year"/>
                     </Form.Group>
                  </Form.Row>
                  <Form.Group controlId="EditProfileComponent.CVV">
                     <Form.Label>Security Code</Form.Label>
-                    <Form.Control style={{ width: "25%" }} type="number" maxlength="3" onInput={checkLength} placeholder="CVV"/>
+                    <Form.Control style={{ width: "25%" }} type="text" maxlength="3" onInput={checkLength} placeholder="CVV"/>
                  </Form.Group>
                  <Button type="submit">Add</Button>
             </Container>
