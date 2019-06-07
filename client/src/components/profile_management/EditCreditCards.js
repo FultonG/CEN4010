e8e9638c01c41ccaec32e2557fb23b142ede3da2
Joel De La Cruz <45545585+joeldlc@users.noreply.github.com>
2019-06-07 09:54:31 -0400

    Added useful resource, database diagram.

--- client/src/components/profile_management/EditCreditCards.js
+++ client/src/components/profile_management/EditCreditCards.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Button, Col, Form} from "react-bootstrap";
 
+// Stick to database diagram: https://drive.google.com/file/d/1SsKnQoe0B_wKTRp6rKocK9oaolqMd1OT/view?usp=sharing
 class EditCreditCards extends Component {
     constructor(props){
         super(props);
