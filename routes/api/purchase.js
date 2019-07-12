d6990a3c1538daec7b0b73a8e3ecbfb9b1091a43
Joel De La Cruz <45545585+joeldlc@users.noreply.github.com>
2019-07-12 00:13:55 -0400

    Add addPurchase

---
+++ routes/api/purchase.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const purchase = require("../../controllers/purchase.js");
+
+router.post("/addPurchase", (req, res) => {
+    purchase.addPurchase(req.body,(status, data = "ok") => res.status(status).send(data));
+});
+
+module.exports = router;
