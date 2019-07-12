5d0915f8e9b4c26099080543a773b10f742baf51
Joel De La Cruz <45545585+joeldlc@users.noreply.github.com>
2019-07-12 00:29:53 -0400

    Add purchase methods for controllers

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -1,7 +1,24 @@
 const mongodbConnection = require("../dbconfig/connection.js"),
     ObjectId = require('mongodb').ObjectId,
     purchase = {
-
+        addPurchase: (data, cb) => {
+            const collection = mongodbConnection.db().collection("Purchase");
+            collection.insertOne(data, function (err, result) {
+                if (!err) {
+                    cb(200, result )
+                } else {
+                    console.log(err);
+                    cb(500, err);
+                }
+            });
+        },
+        updatePurchase: (data, cb) => {
+            const collection = mongodbConnection.db().collection("Purchase");
+            collection.updateOne({ _id: new ObjectId(data.primaryKeys) },
+                { $set: data.updates }, function (err, result) {
+                    !err ? cb(200, result) : cb(500, err);
+                });
+        },
     };
 
 module.exports = purchase;
