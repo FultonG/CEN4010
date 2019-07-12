652b80b0ce9808806dd7c429014b6232ecf0e46d
celif <cfeliciano8791@gmail.com>
2019-07-12 18:16:42 -0400

    Added getPurchase and getPurchases to API routes

--- routes/api/purchase.js
+++ routes/api/purchase.js
@@ -5,6 +5,14 @@ router.post("/addPurchase", (req, res) => {
     purchase.addPurchase(req.body,(status, data = "ok") => res.status(status).send(data));
 });
 
+router.post("/getPurchase", (req, res) => {
+    purchase.getPurchase(req.body,(status, data = "ok") => res.status(status).send(data));
+});
+
+router.post("/getPurchases", (req, res) => {
+    purchase.getPurchases(req.body,(status, data = "ok") => res.status(status).send(data));
+});
+
 router.post("/updatePurchase", (req, res) => {
     purchase.updatePurchase(req.body,(status, data = "ok") => res.status(status).send(data));
 });
