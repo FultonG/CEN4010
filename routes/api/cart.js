014ca7fbff6dd99534524c43f2b9633c9e957488
Joel De La Cruz <45545585+joeldlc@users.noreply.github.com>
2019-07-13 02:00:47 -0400

    Add getCartForUser

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -2,15 +2,19 @@ const router = require('express').Router();
 const cart = require("../../controllers/cart.js");
 
 router.post("/addToCart", (req, res) => {
-    purchase.addToCart(req.body,(status, data = "ok") => res.status(status).send(data));
+    cart.addToCart(req.body,(status, data = "ok") => res.status(status).send(data));
 });
 
 router.post("/updateCartBookQuantiy", (req, res) => {
-    purchase.updateCartBookQuantiy(req.body,(status, data = "ok") => res.status(status).send(data));
+    cart.updateCartBookQuantiy(req.body,(status, data = "ok") => res.status(status).send(data));
 });
 
 router.post("/removeFromCart", (req, res) => {
-    purchase.removeFromCart(req.body,(status, data = "ok") => res.status(status).send(data));
+    cart.removeFromCart(req.body,(status, data = "ok") => res.status(status).send(data));
+});
+
+router.post("/getCartForUser", (req, res) => {
+    cart.geetCartForUser(req.body,(status, data = "ok") => res.status(status).send(data));
 });
 
 module.exports = router;
